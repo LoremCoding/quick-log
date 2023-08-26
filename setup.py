838a55f1f8cf4e4dc90dec610d0efad50fb59b2a
LoremCoding <134689387+LoremCoding@users.noreply.github.com>
2023-08-26 16:26:57 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="quicklog",
-    version="0.2",
+    version="0.2.1",
     description="Minimal easy logging",
     author="Issam MHADHBI",
     author_email="mhadhbiXissam@email.com",
