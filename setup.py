60c100d687d17e52e7f77fdd3b0202cd87dcb540
LoremCoding <134689387+LoremCoding@users.noreply.github.com>
2023-08-26 16:22:17 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="quicklog",
+    version="0.1",
+    description="Minimal easy logging",
+    author="Issam MHADHBI",
+    author_email="mhadhbiXissam@email.com",
+    packages=find_packages(),  # Automatically discover all packages
+    install_requires=[],
+    license="MIT"
+)
